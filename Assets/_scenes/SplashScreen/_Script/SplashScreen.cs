b4fa1d74c9b34e8238285238cc002982c011fec6
alifTC12 <alif.sip@gmail.com>
2015-03-26 15:54:55 +0700

    Splash Screen hiearki diraktori

---
+++ Assets/_scenes/SplashScreen/_Script/SplashScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	public int delayTime;
+	public string NextLevel;
+	// Use this for initialization
+	void Start () 
+	{
+		guiTexture.pixelInset = new Rect (0, 0, Screen.width, Screen.height);
+		StartCoroutine (LoadNextScenes ());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	IEnumerator LoadNextScenes()
+	{
+		yield return new WaitForSeconds (delayTime);
+		if (NextLevel != "")
+						Application.LoadLevel(NextLevel);
+	}
+}
